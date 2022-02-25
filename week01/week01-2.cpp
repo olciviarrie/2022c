d9b5a6139715ebbf0a7e58563c6651cca5fd6b53
申凱菁 <91250041+olciviarrie@users.noreply.github.com>
2022-02-25 12:11:04 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("½m²ßÅª¤J¦r¤¸,¦r¦ê: ");
+    char c;
+    scanf("%c",&c);
+    printf("Åª¨ì¦r¤¸: %c\n",c);
+
+    char line[1000];
+    scanf("%s",line);
+    printf("Åª¨ì¦r¦ê: %s\n",line);
+
+    return 0;
+}
---
+++ week01/week03-1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+char line[1000];
+int main()
+{
+	int total=0;
+	//for(int k=0;k<4;k++)
+	while(scanf("%s",line)==1)
+	{
+		//scanf("%s",line);
+		int ans=0;
+
+		int N=strlen(line);
+	    for(int i=0;i<N;i++)
+		{
+			if(line[i]=='2') ans++;
+		}
+		printf("%d\n",ans);
+		total+=ans;
+	}
+	printf("Total: %d\n",total);
+}
